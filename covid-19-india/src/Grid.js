d3284263c027d48f2e82a53db9639825d232ce1d
Ashish Kumar Singh <ashishkumarsinghh@outlook.com>
2020-03-22 10:15:14 +0530

    remove semantic-ui, add bootstrap

--- covid-19-india/src/Grid.js
+++ covid-19-india/src/Grid.js
@@ -1,50 +1,44 @@
 import React from "react";
-import { Grid, Image } from "semantic-ui-react";
 import Patient from "./Patient";
-
-const SiteGrid = props => (
-  <Grid textAlign="center">
-    <Grid.Row columns={1}>
-      <Grid.Column>
+import { Container, Row, Col, Image } from "react-bootstrap";
+const Site = props => (
+  <Container fluid>
+    <Row>
+      <Col>
         <h1 className="masthead">Covid-19 - India</h1>
-        <Image src="covid.jpg" size="tiny" centered />
-      </Grid.Column>
-    </Grid.Row>
-    <div className="ui divider"></div>
-    <Grid.Row columns={3}>
-      <Grid.Column>
-        <h3>Total Deaths</h3>
+        <Image src="covid.jpg" alt="logo" />
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <h3> Deaths</h3>
         <h1 className="deaths">{props.deaths}</h1>
         <h5 className="deaths">
           Fatality Rate : {((props.deaths * 100) / props.cases).toFixed(2)}%
         </h5>
-      </Grid.Column>
-      <Grid.Column>
-        <h3>Total Cases</h3>
+      </Col>
+      <Col>
+        <h3>Confirmed</h3>
         <h1>{props.cases}</h1>
         <h5>Official : {props.official}</h5>
-      </Grid.Column>
+      </Col>
 
-      <Grid.Column>
-        <h3>Total Recovered</h3>
+      <Col>
+        <h3> Recovered</h3>
         <h1 className="recovered">{props.recovered}</h1>
         <h5 className="recovered">
           Recovery Rate : {((props.recovered * 100) / props.cases).toFixed(2)}%
         </h5>
-      </Grid.Column>
-    </Grid.Row>
-    <div className="ui divider"></div>
-    <Grid.Row columns={4}>
+      </Col>
+    </Row>
+    <Row>
       {props.details &&
         props.details.map(item => (
-          <Grid.Column>
+          <Col xs={6} md={4}>
             <Patient details={item} />
-          </Grid.Column>
+          </Col>
         ))}
-      {/* <Grid.Column>
-        {props.details && <Patient details={props.details[0]} />}
-      </Grid.Column> */}
-    </Grid.Row>
-  </Grid>
+    </Row>
+  </Container>
 );
-export default SiteGrid;
+export default Site;
--- covid-19-india/src/Patient.js
+++ covid-19-india/src/Patient.js
@@ -1,29 +1,16 @@
 import React from "react";
-
-import { Card } from "semantic-ui-react";
-
 export default function Patient(props) {
   return (
     <React.Fragment>
-      <Card>
-        <Card.Content>
-          <Card.Header>
-            Patient ID : {props.details && props.details.patientId}
-          </Card.Header>
-          <Card.Meta>
-            <span className="date">
-              Reported on {props.details && props.details.reportedOn}
-            </span>
-          </Card.Meta>
-          <Card.Description>
-            {props.details && props.details.city} ,{" "}
-            {props.details && props.details.state}
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          {props.details && props.details.notes}
-        </Card.Content>
-      </Card>
+      <div className="patientDetail">
+        <h2>Patient ID : {props.details && props.details.patientId}</h2>
+        <p>Reported on {props.details && props.details.reportedOn}</p>
+        <p>
+          {props.details && props.details.city} ,
+          {props.details && props.details.state}
+        </p>
+        <p>{props.details && props.details.notes}</p>
+      </div>
     </React.Fragment>
   );
 }
